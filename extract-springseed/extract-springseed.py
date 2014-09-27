3073eb4d9da2e3fcb3545889471de77b5c48e44c
paulgkramer <paul.kramer@gmail.com>
2014-09-27 21:02:07 +0100

    functions built into the script

--- extract-springseed/extract-springseed.py
+++ extract-springseed/extract-springseed.py
@@ -5,7 +5,6 @@ import os.path
 
 Location = "/Users/Paul/GitHub/Python-Learnings/extract-springseed/extract-springseed-files/"
 output_location = "/Users/Paul/GitHub/Python-Learnings/extract-springseed/output/"
-notebook_dict = {}
 
 
 def write(filename,content):
@@ -25,45 +24,49 @@ def write(filename,content):
 
 
 
-# def create_notebook():
+def create_notebook():
 
-for filename in listdir(Location):
+  notebook_dict = {}
 
-  extension = os.path.splitext(filename)[1]
+  for filename in listdir(Location):
 
-  if extension == ".list":
-    list_filename = Location + filename
-    notebook_file_contents = open(list_filename).read()
-    notebook_id = eval(notebook_file_contents)['id']
-    notebook_name = eval(notebook_file_contents)['name']
-    notebook_dir = output_location + notebook_name
+    extension = os.path.splitext(filename)[1]
 
-    if not os.path.exists(notebook_dir):
-      os.makedirs(notebook_dir)
-      notebook_dict[notebook_id] = notebook_name
+    if extension == ".list":
+      list_filename = Location + filename
+      notebook_file_contents = open(list_filename).read()
+      notebook_id = eval(notebook_file_contents)['id']
+      notebook_name = eval(notebook_file_contents)['name']
+      notebook_dir = output_location + notebook_name
 
+      if not os.path.exists(notebook_dir):
+        os.makedirs(notebook_dir)
+        notebook_dict[notebook_id] = notebook_name
 
-
-# def create_note():
-
-for filename in listdir(Location):
-
-  extension = os.path.splitext(filename)[1]
-
-  if extension == ".note":
-    json_filename = Location + filename
-    json_file_contents = open(json_filename).read()
-
-    str_content = eval(json_file_contents)['content']
-    notebook_id = eval(json_file_contents)['notebook']
-    note_name = eval(json_file_contents)['name'].replace('/', '')
-    note_directory = [val for key, val in notebook_dict.items() if notebook_id in key]
+  return notebook_dict
 
 
-    output_filename = output_location + note_directory[0] + "/" + note_name
-
-    write(output_filename,str_content)
 
 
-  # create_notebook()
-  # create_note()
+def create_note(notebook_dict):
+
+  for filename in listdir(Location):
+
+    extension = os.path.splitext(filename)[1]
+
+    if extension == ".note":
+      json_filename = Location + filename
+      json_file_contents = open(json_filename).read()
+
+      str_content = eval(json_file_contents)['content']
+      notebook_id = eval(json_file_contents)['notebook']
+      note_name = eval(json_file_contents)['name'].replace('/', '')
+      note_directory = [val for key, val in notebook_dict.items() if notebook_id in key]
+
+
+      output_filename = output_location + note_directory[0] + "/" + note_name
+
+      write(output_filename,str_content)
+
+
+create_note(create_notebook())
